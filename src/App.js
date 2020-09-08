b59214763f557beb3b4cd14b897393689d47cec7
mihirverma7781 <mihirv7781@gmail.com>
2020-09-09 01:57:20 +0530

    Image splash (practice project)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+const [value,setValue]= useState("cars")
+const[results,setResults]= useState([])
+// 8vWwx8PiqhV9-tHJicVSZNssxYFgi0ZcYTyFRqOBxCk
+const fetchImages=()=>{
+  fetch(`http://api.unsplash.com/search/photos?client_id=8vWwx8PiqhV9-tHJicVSZNssxYFgi0ZcYTyFRqOBxCk&query=${value}&orientation=squarish&per_page=30`)
+  .then(res=>res.json())
+  .then(data=>{
+setResults(data.results)
+  })
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="mydiv">
+        <span className="head">Image<text>Splash</text></span>
+        <div className="inputsearch">
+        <span className="tail">Search an image </span>
+        <input className="searchInput" type="text"  value={value} onChange={(e)=>setValue(e.target.value)} />
+        <button onClick={()=>fetchImages()} className="find">Find</button>
+        </div>
+      </div>
+      <div className="cat">
+  <h1>Category  &#10159;  <text>{value}</text></h1>
+     </div>
+
+   <div className="gallery">
+  
+{
+  results.map((item)=>{
+    return <img className="item" key={item.id} src={item.urls.regular}/>
+  })
+}
+   </div>
+
+
     </div>
   );
 }
